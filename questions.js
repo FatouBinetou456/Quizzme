2aea358c3b0a5a733c220b691f3252ad92f09538
FatouBinetou456 <102521775+FatouBinetou456@users.noreply.github.com>
2023-11-14 16:42:27 +0000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,183 @@
+let questions = [
+    {
+        numb:1,
+        question: "Que signifie HTML?",
+        answer: "C.Hyper Text Markup Language",
+        options: ["A. Hyper Type Multi Language",
+                "B. Hyper Text Multiple Language",
+            "C.Hyper Text Markup Language",
+        "D. Home Text Multi Language"]
+    },
+    {
+        numb:2,
+        question: "Quelle balise HTML est utilisée pour créer une liste non ordonnée ?",
+        answer: "A. 'ul'",
+        options: ["A. 'ul'",
+            "B. 'ol'",
+            "C. 'li'",
+            "D. 'div'"]
+    },
+    {
+        numb:3,
+        question: "Quelle propriété CSS est utilisée pour définir la couleur du texte ?",
+        answer: "C. color",
+        options: ["A. text-color",
+        "B. font-color",
+        "C. color",
+        "D. text-style"]
+    },
+    {
+        numb:4,
+        question: "Quelle balise HTML est utilisée pour créer un lien hypertexte ?",
+        answer: "C. 'a'",
+        options: ["A. 'link'",
+        "B. 'href'",
+        "C. 'a'",
+        "D. 'url'"]
+    },
+    {
+        numb:5,
+        question: "Quelle propriété CSS est utilisée pour définir la taille de la police ?",
+        answer: "B. font-size",
+        options: ["A. text-size",
+            "B. font-size",
+            "C. font-height",
+            "D. size"]
+    },
+    {
+        numb:6,
+        question: "Comment déclarer une variable en JavaScript ?",
+        answer: "C. var x",
+        options: ["A. variable x",
+            "B. let x",
+            "C. var x",
+            "D. x = variable",]
+    },
+    {
+        numb:7,
+        question: "Quelle balise HTML est utilisée pour créer un paragraphe ?",
+        answer: "B. 'p'",
+        options: ["A. 'paragraph'",
+            "B. 'p'",
+            "C. para'",
+            "D. 'pg'",]
+    },
+    {
+        numb:8,
+        question: "Quelle propriété CSS permet de définir la couleur de fond ?",
+        answer: "A. background-color",
+        options: ["A. background-color",
+            "B. color-background",
+            "C. bg-color",
+            "D. element-background"]
+    },
+    {
+        numb:9,
+        question: "Comment ajoutez-vous un commentaire en JavaScript ?",
+        answer: "B. '// Ceci est un commentaire'",
+        options: ["A. '/* Ceci est un commentaire */'",
+        "B. '// Ceci est un commentaire'",
+        "C. '# Ceci est un commentaire'",
+        "D. '<-- Ceci est un commentaire -->'"]
+    },
+    {
+        numb:10,
+        question: "Quelle balise HTML est utilisée pour créer une liste ordonnée ?",
+        answer: "A. 'ol'",
+        options: ["A. 'ol'",
+            "B. 'ul'",
+            "C. 'li'",
+            "D. 'list'"]
+    },
+    {
+    numb:11,
+    question: "Quelle propriété CSS permet de définir la largeur ?",
+    answer: "A. width",
+    options: ["A. width",
+    "B. element-width",
+    "C. size",
+    "D. element-size"]
+},
+{
+    numb:12,
+    question: "Quelle balise HTML est utilisée pour créer un titre de page ?",
+    answer: "C. 'h1'",
+    options: ["A. 'header'",
+    "B. 'title'",
+    "C. 'h1'",
+    "D. 'heading'"]
+},
+{
+    numb:13,
+    question: "Quelle propriété CSS permet de mettre en italique le texte",
+    answer: "A. font-style: italic",
+    options: ["A. font-style: italic",
+    "B. text-italic: true;",
+    "C. italic-text: on;",
+    "D. style: italic;"]
+},
+{
+    numb:14,
+    question: " Quel langage pour fixer la structure et le contenu du site ?",
+    answer: 'A. HTML',
+    options: ['A. HTML"',
+    'B. CSS',
+    'C. Javascript',
+    'D. PHP']
+},
+{
+    numb:15,
+    question: "Quelle balise HTML est utilisée pour créer un tableau ?",
+    answer: "A. 'table'",
+    options: ["A. 'table'",
+    "B. 'tab'",
+    "C. 'tr'",
+    "D. 'td'"]
+},
+{
+    numb:16,
+    question: "Quelle propriété CSS permet de définir la marge extérieure ?",
+    answer: "B. margin",
+    options: ["A. padding",
+    "B. margin",
+    "C. spacing",
+    "D. outer-margin"]
+},
+{
+    numb:17,
+    question: "Quel langage sert à styliser et mettre en forme une page web ? ",
+    answer: "C. CSS'",
+    options: ["A. Java'",
+    "B. HTML",
+    "C. CSS",
+    "D. C++"]
+},
+{
+    numb:18,
+    question: "Quelle balise HTML pour d'insérer une image ?",
+    answer: "A. 'img'",
+    options: ["A. 'img'",
+    "B. 'picture'",
+    "C. 'image'",
+    "D. 'photo'"]
+},
+{
+    numb:19,
+    question: "Quelle propriété CSS pour définir la bordure ?",
+    answer: "A. border",
+    options: ["A. border",
+        "B. element-border",
+        "C. border-style",
+        "D. outline"]
+},
+{
+    numb:20,
+    question: "Que signifie CSS",
+    answer: "D. Cascading Style Sheets",
+    options: ["A. Concise Styling Syntax",
+    "B. Concise Styling Syntax",
+    "C. Control of Stylish Structures",
+    "D. Cascading Style Sheets"
+]
+}
+]
